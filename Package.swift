0fab341088a87d557729ed637c79e474a127692c
Pavel Procházka <pprochazka72@gmail.com>
2017-02-04 10:42:35 +0100

    Remove dependency on TODO-Foudantion

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "TODO-Backend",
 	dependencies: [
-		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
-		.Package(url: "https://github.com/pajapro/TODO-Foundation.git", majorVersion: 1, minor: 0)
+		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
 	]
 )
