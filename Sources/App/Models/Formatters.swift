47e506e04f5c495c35aeca295d946128a38a1f4a
Pavel Procházka <pprochazka72@gmail.com>
2017-02-04 12:54:39 +0100

    Add date formatter

---
+++ Sources/App/Models/Formatters.swift
@@ -0,0 +1,19 @@
+//
+//  Formatters.swift
+//  TODO-Foundation
+//
+//  Created by Pavel Procházka on 26/01/2017.
+//
+//
+
+import Foundation
+
+public extension DateFormatter {
+	
+	static func configuredDateFormatter() -> DateFormatter {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "EEE, dd MMM yyyy hh:mm:ss"
+		dateFormatter.locale = Locale.current
+		return dateFormatter
+	}
+}
