785839eb5cfcb6b0b9c86b4a826e08c131b23934
Pavel Procházka <pprochazka72@gmail.com>
2017-02-04 12:03:26 +0100

    Add JSON keys struct

---
+++ Sources/App/Models/JSONkeys.swift
@@ -0,0 +1,16 @@
+//
+//  JSONkeys.swift
+//  TODO-Foundation
+//
+//  Created by Pavel Procházka on 26/01/2017.
+//
+//
+
+import Foundation
+
+struct JSONKeys {
+	static let title	= "title"
+	static let tasks	= "tasks"
+	static let priority = "priority"
+	static let dueDate	= "dueDate"
+}
