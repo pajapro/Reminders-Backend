eb2b131286fe1fca262b1f83d3cf8964e263d674
Pavel Procházka <pprochazka72@gmail.com>
2017-02-05 17:02:13 +0100

    Add dependency on PostreSQL provider

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "TODO-Backend",
 	dependencies: [
-		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+		.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1)
 	]
 )
