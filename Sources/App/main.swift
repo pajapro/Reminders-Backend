96c14eda24298c029822138d0c44b1eb8b0a9c92
Pavel Procházka <pprochazka72@gmail.com>
2017-02-05 17:03:17 +0100

    Create “dbversion” endpoint

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -5,21 +5,41 @@ import VaporPostgreSQL
 let drop = Droplet()
 try drop.addProvider(VaporPostgreSQL.Provider.self)
 
+/// MARK: - Utility endpoinds
+
 // Root request
 drop.get() { _ in
 	return try JSON(node : ["message": "More awesomeness coming soon..."])
 }
 
-// GET for resource (hello/resource)
+// GET util resources
+drop.get("dbversion") { _ in
+	if let db = drop.database?.driver as? PostgreSQLDriver {
+		let version = try db.raw("SELECT version()")
+		return try JSON(node: version)
+	} else {
+		return "No database connection"
+	}
+}
+
+drop.get("model") { _ in
+	let task = Task(title: "Greet me!", priority: .medium, dueDate: Date())
+	return try task.makeJSON()
+}
+
+/// MARK: - Test endpoinds
+
+// GET for resource at String
 drop.get("resource", String.self) { request, name in
 	return "So you want \(name)?!"
 }
 
+// GET for resource at Int
 drop.get("drink", Int.self) { request, beers in
 	return "Are you sure you want to drink \(beers) beers?"
 }
 
-// GET with params (hello?param=value)
+// GET resource with params (hello?param=value)
 drop.get("hello") { request in
 	let name = request.data["name"]?.string ?? "stranger"
 	return "Hello \(name), how you doing?"
