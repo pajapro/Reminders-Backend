848b628a2e9ac516e2e5dd997ee72d72ddbb801d
Pavel Procházka <pprochazka72@gmail.com>
2017-02-07 19:57:57 +0100

    Rename identifiers

---
+++ Sources/App/Models/Identifiers.swift
@@ -0,0 +1,22 @@
+//
+//  JSONkeys.swift
+//  TODO-Foundation
+//
+//  Created by Pavel Procházka on 26/01/2017.
+//
+//
+
+import Foundation
+
+struct Identifiers {
+	
+	// NOTE: keys have to be all lowercase, because Postgres stores them so.
+	
+	static let id			= "id"
+	static let title		= "title"
+	static let tasks		= "tasks"
+	static let priority		= "priority"
+	static let dueDate		= "duedate"
+	static let creationDate	= "creationdate"
+	static let isDone		= "isdone"
+}
