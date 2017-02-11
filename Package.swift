6062d6e207eab857a61cca08baf19dca2bfab26b
Pavel Procházka <pprochazka72@gmail.com>
2017-02-11 21:26:50 +0100

    Rename application

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "TODO-Backend",
+    name: "Reminders-Backend",
 	dependencies: [
 		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
 		.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1)
