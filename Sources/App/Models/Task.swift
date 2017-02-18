449015fa6c6da451c6180a35c5fe2496edeff231
Pavel Procházka <pprochazka72@gmail.com>
2017-02-18 10:31:27 +0100

    Remove nil as try will assign instead automatically

--- Sources/App/Models/Task.swift
+++ Sources/App/Models/Task.swift
@@ -82,7 +82,7 @@ extension Task: NodeInitializable {
 		self.id = try node.extract(Identifiers.id)
 		self.title = try node.extract(Identifiers.title)
 		self.priority = try node.extract(Identifiers.priority, transform: Priority.priority) ?? .none
-		self.dueDate = try node.extract(Identifiers.dueDate, transform: Date.date) ?? nil
+		self.dueDate = try node.extract(Identifiers.dueDate, transform: Date.date) 
 		self.creationDate = try node.extract(Identifiers.creationDate, transform: Date.date)
 		self.isDone = try node.extract(Identifiers.isDone)
 		self.listId = try node.extract(Identifiers.listId)
