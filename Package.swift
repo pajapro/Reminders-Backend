3400b69d48456ae8de9a9d3f40bd54f13a4ca31d
Pavel Procházka <pprochazka72@gmail.com>
2017-03-08 20:06:02 +0100

    Bump Vapor version to 1.5.9

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Reminders-Backend",
 	dependencies: [
-		.Package(url: "https://github.com/vapor/vapor.git", "1.5.8"),
+		.Package(url: "https://github.com/vapor/vapor.git", "1.5.9"),
 		.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1)
 	]
 )
