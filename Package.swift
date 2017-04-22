eb2ae71dfa47be23e375e19fd08dd227f32acadc
Pavel Procházka <pprochazka72@gmail.com>
2017-04-22 10:00:21 +0200

    Add SwiftyBeaver

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "Reminders-Backend",
 	dependencies: [
 		.Package(url: "https://github.com/vapor/vapor.git", "1.5.15"),
-		.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1)
+		.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1),
+		.Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver-Vapor.git", majorVersion: 1),
 	]
 )
