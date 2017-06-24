80ccc777a3d68892f8f3384ccfac970c06ea4701
Pavel Procházka <pprochazka72@gmail.com>
2017-06-24 15:58:45 +0200

    Update dependencies to Vapor 2 compatible

--- Package.swift
+++ Package.swift
@@ -3,8 +3,12 @@ import PackageDescription
 let package = Package(
     name: "Reminders-Backend",
 	dependencies: [
-		.Package(url: "https://github.com/vapor/vapor.git", "1.5.15"),
-		.Package(url: "https://github.com/vapor/postgresql-provider", majorVersion: 1, minor: 1),
-		.Package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver-Vapor.git", majorVersion: 1),
+		.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
+		.Package(url: "https://github.com/vapor/auth-provider.git", majorVersion: 1),
+		.Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
+		.Package(url: "https://github.com/vapor/validation-provider.git", majorVersion: 1),
+        .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1),
+		.Package(url: "https://github.com/vapor-community/postgresql-provider", majorVersion: 2),
+		.Package(url: "https://github.com/vapor-community/swiftybeaver-provider", majorVersion: 1),
 	]
 )
