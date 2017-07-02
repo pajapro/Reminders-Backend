2eda951d3f1b64aaf3b065c40091f18a9cc346c6
Pavel Procházka <pprochazka72@gmail.com>
2017-07-02 17:46:15 +0200

    Migrate main.swift to Vapor 2

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,51 +1,76 @@
 import Vapor
-import Foundation
-import VaporPostgreSQL
-import HTTP
-import Auth
-import SwiftyBeaverVapor
-import SwiftyBeaver
+import PostgreSQLProvider
+import AuthProvider
+import LeafProvider
+import Sessions
 
-let drop = Droplet()
+let config = try Config()
 
-// Invoke `prepare` function to create corresponding tables
-drop.preparations.append(List.self)
-drop.preparations.append(Task.self)
-drop.preparations.append(User.self)
+// MARK: Providers
 
-// Add authentication middleware
-drop.middleware.append(AuthMiddleware(user: User.self))
-
-// Add version middleware
-drop.middleware.append(VersionMiddleware())
-
-// Create protect middleware to require authentication on certain endpoints
-let protectMiddleware = ProtectMiddleware(error: Abort.custom(status: .forbidden, message: "Invalid credentials."))
-
-// Connect to PostgreSQL DB
 do {
-	try drop.addProvider(VaporPostgreSQL.Provider.self)
+	// Connect to PostgreSQL DB
+	try config.addProvider(PostgreSQLProvider.Provider.self)
 } catch {
-	print("Error adding provider: \(error)")
+	print("Error adding PostgreSQL provider: \(error)")
 }
 
-// Add SwiftyBeaver logging
-let console = ConsoleDestination()  // log to Xcode Console in color
-let file = FileDestination()		// log to file in color
-file.logFileURL = URL(fileURLWithPath: "/tmp/VaporLogs.log")
-let sbProvider = SwiftyBeaverProvider(destinations: [console, file])
-drop.addProvider(sbProvider)
+do {
+	// Authentication provider
+	try config.addProvider(AuthProvider.Provider.self)
+} catch {
+	print("Error adding Auth provider: \(error)")
+}
+
+do {
+	// Templating provider
+	try config.addProvider(LeafProvider.Provider.self)
+} catch {
+	print("Error adding Leaf provider: \(error)")
+}
+
+do {
+} catch {
+}
+
+// MARK: Database setup
+
+config.preparations.append(User.self)
+config.preparations.append(List.self)
+config.preparations.append(Task.self)
+
+// MARK: Middleware (see Config/droplet.json)
+
+// Create in-memory session middleware https://docs.vapor.codes/2.0/sessions/sessions/
+let memory = MemorySessions()
+let sessionsMiddleware = SessionsMiddleware(memory)
+config.addConfigurable(middleware: sessionsMiddleware, name: "sessions")
+
+// Create persist middleware for persisting user once authenticated https://docs.vapor.codes/2.0/auth/persist/#persist
+config.addConfigurable(middleware: PersistMiddleware(User.self), name: "user-persist")
+
+// Create version middleware
+config.addConfigurable(middleware: VersionMiddleware(), name: "version")
+
+// Create password authentication middleware to require authenticated user on certain endpoints
+let passwordMiddleware = PasswordAuthenticationMiddleware(User.self) // It's not added to `config` as it's not required for all endpoints!
+
+// MARK: Droplet
+
+let drop = try Droplet(config)
 
 // Disable caching in order to avoid recompling the app for HTML & CSS tweaks
 (drop.view as? LeafRenderer)?.stem.cache = nil
 
+// MARK: Routes
+
 // Add (protected) lists routes
 let listsController = ListsController()
-listsController.addRoutes(to: drop, with: protectMiddleware)
+listsController.addRoutes(to: drop, with: [passwordMiddleware])
 
 // Add (protected) tasks routes
 let tasksController = TasksController()
-tasksController.addRoutes(drop: drop, with: protectMiddleware)
+tasksController.addRoutes(drop: drop, with: [passwordMiddleware])
 
 // Add (unprotected) utility routes
 let utilityController = UtilityController()
@@ -56,4 +81,4 @@ let usersController = UsersController()
 usersController.addRoutes(drop: drop)
 
 // Start Vapor service
-drop.run()
+try drop.run()
