90029609bc7848ccd7d8e6fc4b16ab755fcec385
Pavel Procházka <pprochazka72@gmail.com>
2017-07-02 17:08:12 +0200

    Update identifiers

--- Sources/App/Utilities/Identifiers.swift
+++ Sources/App/Utilities/Identifiers.swift
@@ -12,19 +12,21 @@ struct Identifiers {
 	
 	// NOTE: keys have to be all lowercase, because Postgres stores them so.
 	
-	static let id			= "id"
-	static let title		= "title"
-	static let tasks		= "tasks"
-	static let priority		= "priority"
-	static let dueDate		= "due_date"
-	static let creationDate	= "creation_date"
-	static let isDone		= "is_done"
-	static let listId		= "list_id"
+	static let id = "id"
+	static let title = "title"
+	static let tasks = "tasks"
+	static let priority = "priority"
+	static let dueDate = "due_date"
+	static let createdAt = "created_at"
+	static let updatedAt = "updated_at"
+	static let isDone = "is_done"
+	static let listId = "list_id"
 	
-	static let name			= "name"
-	static let email		= "email"
-	static let password		= "password"
-	static let userId		= "user_id"
+	static let name = "name"
+	static let email = "email"
+	static let password = "password"
+	static let userId = "user_id"
+	static let token = "token"
 	
 	// HTTP Content-Type
 	static let json = "application/json"
