9146b7b34aad769323a39187b665290baa68c2de
Pavel Procházka <pprochazka72@gmail.com>
2018-12-27 23:34:46 +0100

    Revert "💡Add UNIQUE constraint to User’s emai column"
    This reverts commit 6902e0be163d7eb2f9488e58b75a47b3c440613f.

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -48,21 +48,7 @@ extension User {
 }
 
 /// Allows `User` to be used as a dynamic migration.
-extension User: Migration  {}
-
-struct UserEmailUniqueConstraint: Migration {
-	typealias Database = PostgreSQLDatabase
-	
-	static func prepare(on conn: PostgreSQLConnection) -> Future<Void> {
-		return Database.update(User.self, on: conn) { builder in
-			builder.unique(on: \.email)
-		}
-	}
-	
-	static func revert(on connection: PostgreSQLConnection) -> Future<Void> {
-		return Future.map(on: connection) {}
-	}
-}
+extension User: Migration { }
 
 /// Allows `User` to be encoded to and decoded from HTTP messages.
 extension User: Content { }
