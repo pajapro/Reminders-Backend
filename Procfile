8b55f4b2f24b96fee78d9f91ff57c0aa9f14331d
Pavel Procházka <pprochazka72@gmail.com>
2019-02-02 22:19:40 +0100

    Make DB configureable

--- Procfile
+++
@@ -1,3 +0,0 @@
-# Heroku configuration
-web: App --env=production --workdir="./"
-web: App --env=production --workdir=./ --config:server.port=$PORT --config:postgresql.url=$DATABASE_URL
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -22,9 +22,16 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
     
     // Register PostgreSQL database
-    let psqlConfig = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "vapor", password: "password")
-    let psqlDatabase = PostgreSQLDatabase(config: psqlConfig)
-    var dbConfig = DatabasesConfig()
+	let psqlConfig: PostgreSQLDatabaseConfig
+	if let url = Environment.get("DATABASE_URL"), let dbConfig = PostgreSQLDatabaseConfig(url: url) { // it will read from this URL in production
+		psqlConfig = dbConfig
+		print("Registered DB from DATABASE_URL")
+	} else { // when environment variable not present, default to local development environment
+		psqlConfig = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "vapor", database: "vapor", password: "password")
+		print("Registered local DB")
+	}
+	let psqlDatabase = PostgreSQLDatabase(config: psqlConfig)
+	var dbConfig = DatabasesConfig()
     /// Register the configured PostgreSQL database to the database config
     dbConfig.add(database: psqlDatabase, as: .psql)
     // Enable logging on the SQLite database
