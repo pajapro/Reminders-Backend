d1a07fc6036118e3b28d12853c115af2dd8ca3c3
Pavel Procházka <pprochazka72@gmail.com>
2019-02-03 08:55:02 +0100

    Upgrade Vapor version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "Reminders-Backend",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.1"),
         
         // 🖋🐘 Swift ORM (queries, models, relations, etc) built on PostgreSQL.
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
